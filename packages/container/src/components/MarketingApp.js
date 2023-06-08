fd7ea0b78844bfdb6f2d13638fdbfc44ec56458a
rnagaraju <rnagaraju@carparts.com>
2023-06-08 19:57:57 +0530

    Initial commit

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,13 @@
+import React, { useEffect , useRef} from 'react'
+import { mount } from 'marketing/MarketingApp'
+
+
+export default () => {
+    const elemRef = useRef(null)
+    useEffect(() => {
+        if(elemRef.current) {
+            mount(elemRef.current)
+        }
+    }, [])
+    return <div ref={elemRef} />
+}
---
+++ packages/marketing/configs/webpack.prod.js
@@ -0,0 +1,5 @@
+const { merge } = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {}
