38f2c9ee4bb865a3406c0739eaf648c346fa54d0
rnagaraju <rnagaraju@carparts.com>
2023-06-09 18:51:40 +0530

    css namespace fix production

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,12 +2,21 @@ import React from 'react'
 import { BrowserRouter} from 'react-router-dom'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
 
 export default () => {
-    return <BrowserRouter>
+    return 
+    <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
         <div>
             <Header />
             <MarketingApp />
         </div>
     </BrowserRouter>
+    </StylesProvider>
+    
 }
